508a9554ee4fa2687feaf9e792866fc2139edbbc
Sachin Patidar <sachinpatidar413@gmail.com>
2025-07-22 16:19:14 +0530

    app changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../context/ThemeContext';
 import { useAuth } from '../context/AuthContext';
-import { hoverSound, clickSound } from '../utils/sounds';
+import { hoverSound, clickSound } from './utils/Sounds';
 import { Button } from "../components/ui/button"
 import ThemeToggle from './ThemeToggle';
 import UserDropdown from './UserDropdown';
